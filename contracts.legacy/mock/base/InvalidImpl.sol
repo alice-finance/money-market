dd19ed543f944a34f71d46d5a944cff5a5134442
Changhwan Choi <changhwan@alice.finance>
2019-09-09 13:09:18 +0900

    Remove legacy code

--- contracts.legacy/mock/base/InvalidImpl.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.5.8;
-
-import "../../base/Base.sol";
-
-contract InvalidImpl is Base {
-    function invalidFunction() public pure returns (bool) {
-        return true;
-    }
-}
